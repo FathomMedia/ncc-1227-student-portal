2d627bf74499b7529cd7133b4d2ebdd76d795635
Mohamed Mukhtar <mukxdev@gmail.com>
2023-05-31 14:19:48 +0300

    separate active applications from inactive

--- pages/applications/index.tsx
+++ pages/applications/index.tsx
@@ -9,6 +9,7 @@ import { useTranslation } from "react-i18next";
 import { ApplicationCard } from "../../components/applications/ApplicationCard";
 import { getStatusOrder } from "../../src/HelperFunctions";
 import { NewApplicationCard } from "../../components/applications/NewApplicationCard";
+import { Status } from "../../src/API";
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { locale } = ctx;
@@ -29,40 +30,80 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 export default function ApplicationsPage() {
   const appContext = useAppContext();
 
+  const activeApplications = appContext.applications.filter(
+    (app) =>
+      app.status === Status.APPROVED ||
+      app.status === Status.ELIGIBLE ||
+      app.status === Status.REVIEW ||
+      app.status === Status.NOT_COMPLETED
+  );
+
+  const pastApplications = appContext.applications.filter(
+    (app) => app.status === Status.REJECTED || app.status === Status.WITHDRAWN
+  );
+
   const { t } = useTranslation("applications");
 
   return (
     <PageComponent title={"Applications"} authRequired>
       <div className="container mx-auto">
+        {!appContext.haveActiveApplication && (
+          <div>
+            <p className="stat-value my-4 text-2xl">New Application</p>
+          </div>
+        )}
+        {activeApplications.length > 0 && (
+          <div>
+            <p className="stat-value my-4 text-2xl">Active Applications</p>
+          </div>
+        )}
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3 [grid-auto-rows:1fr]">
           {!appContext.haveActiveApplication && (
-            // <Link
-            //   href={"../applications/new-application"}
-            //   className="duration-200 shadow stats hover:cursor-pointer hover:scale-105 bg-white"
-            // >
-            //   <div className="flex flex-col items-center justify-center stat">
-            //     <div className="prose">
-            //       <h3>{t("createNewApplication")}</h3>
-            //     </div>
-            //   </div>
-            // </Link>
             <Link href={"../applications/new-application"}>
               <NewApplicationCard></NewApplicationCard>
             </Link>
           )}
-          {appContext.applications
-            .sort((a, b) => {
-              if (a.status && b.status) {
-                if (getStatusOrder(b.status) > getStatusOrder(a.status))
-                  return 1;
-                if (getStatusOrder(b.status) < getStatusOrder(a.status))
-                  return -1;
-              }
-              return 0;
-            })
-            .map((application) => (
-              <ApplicationCard key={application.id} application={application} />
-            ))}
+          {appContext.haveActiveApplication &&
+            activeApplications
+              .sort((a, b) => {
+                if (a.status && b.status) {
+                  if (getStatusOrder(b.status) > getStatusOrder(a.status))
+                    return 1;
+                  if (getStatusOrder(b.status) < getStatusOrder(a.status))
+                    return -1;
+                }
+                return 0;
+              })
+              .map((application) => (
+                <ApplicationCard
+                  key={application.id}
+                  application={application}
+                />
+              ))}
+        </div>
+        {pastApplications.length > 0 && (
+          <div>
+            <p className="stat-value my-4 text-2xl">Past Applications</p>
+          </div>
+        )}
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3 [grid-auto-rows:1fr]">
+          {pastApplications.length > 0 &&
+            pastApplications
+              .sort((a, b) => {
+                if (a.status && b.status) {
+                  if (getStatusOrder(b.status) > getStatusOrder(a.status))
+                    return 1;
+                  if (getStatusOrder(b.status) < getStatusOrder(a.status))
+                    return -1;
+                }
+                return 0;
+              })
+              .map((application) => (
+                <ApplicationCard
+                  key={application.id}
+                  application={application}
+                />
+              ))}
         </div>
       </div>
     </PageComponent>
